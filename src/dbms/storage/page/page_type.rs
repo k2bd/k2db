a8ead13d0f350793b1169f26300fe06e74ae5b5b
Kevin Duff <kevinkelduff@gmail.com>
2023-04-14 09:21:40 +0000

    very wip

--- src/dbms/storage/page/page_type.rs
+++ src/dbms/storage/page/page_type.rs
@@ -1,25 +1,23 @@
 #[cfg(test)]
 use mockall::automock;
 
-use std::sync::RwLock;
+use std::sync::{RwLock, RwLockReadGuard};
 
 const PAGE_SIZE: usize = 4096;
 
 #[cfg_attr(test, automock)]
 pub trait IPage {
-    /// Get the whole contents of the page
-    fn get_data(&self) -> [u8; PAGE_SIZE];
+    /// Get a readable copy of the page's data
+    fn get_data(&self) -> RwLockReadGuard<'_, [u8; PAGE_SIZE]>;
     /// Set the whole content of the page, and set the page to dirty
     fn set_data(&mut self, data: [u8; PAGE_SIZE]);
-    /// Read a slice of the page, starting from the given offset
-    fn read_data(&self, offset: usize, size: usize) -> Vec<u8>;
     /// Write a slice of the page, starting from the given offset, and set the
     /// page to dirty
     fn write_data(&mut self, offset: usize, data: &[u8]);
     /// Get the page ID
     fn get_page_id(&self) -> usize;
     /// Get whether the page is dirty
-    fn is_dirty(&self) -> bool;
+    fn is_dirty(&self) -> RwLockReadGuard<'_, bool>;
     /// Set the page to dirty
     fn set_dirty(&mut self);
     /// Set the page to clean
@@ -27,21 +25,21 @@ pub trait IPage {
     /// Increase the pin count of the page by 1
     fn increase_pin_count(&mut self);
     /// Get the pin count of the page
-    fn get_pin_count(&self) -> usize;
+    fn get_pin_count(&self) -> RwLockReadGuard<'_, usize>;
 }
 
-struct Page {
+pub struct Page {
     data: RwLock<[u8; PAGE_SIZE]>,
-    page_id: RwLock<usize>,
+    page_id: usize,
     pin_count: RwLock<usize>,
     is_dirty: RwLock<bool>,
 }
 
 impl Page {
-    fn new(page_id: usize) -> Page {
+    pub fn new(page_id: usize) -> Page {
         Page {
             data: RwLock::new([0; PAGE_SIZE]),
-            page_id: RwLock::new(page_id),
+            page_id,
             pin_count: RwLock::new(0),
             is_dirty: RwLock::new(false),
         }
@@ -49,21 +47,13 @@ impl Page {
 }
 
 impl IPage for Page {
-    fn get_data(&self) -> [u8; PAGE_SIZE] {
+    fn get_data(&self) -> RwLockReadGuard<'_, [u8; PAGE_SIZE]> {
         let data = self.data.read().unwrap();
-        *data
+        data
     }
 
     fn set_data(&mut self, data: [u8; PAGE_SIZE]) {
-        let mut page_data = self.data.write().unwrap();
-        let mut is_dirty = self.is_dirty.write().unwrap();
-        *is_dirty = true;
-        *page_data = data;
-    }
-
-    fn read_data(&self, offset: usize, size: usize) -> Vec<u8> {
-        let data = self.data.read().unwrap();
-        data[offset..offset + size].to_vec()
+        self.write_data(0, &data)
     }
 
     fn write_data(&mut self, offset: usize, data: &[u8]) {
@@ -74,13 +64,12 @@ impl IPage for Page {
     }
 
     fn get_page_id(&self) -> usize {
-        let page_id = self.page_id.read().unwrap();
-        *page_id
+        self.page_id
     }
 
-    fn is_dirty(&self) -> bool {
+    fn is_dirty(&self) -> RwLockReadGuard<'_, bool> {
         let is_dirty = self.is_dirty.read().unwrap();
-        *is_dirty
+        is_dirty
     }
 
     fn increase_pin_count(&mut self) {
@@ -88,9 +77,9 @@ impl IPage for Page {
         *pin_count += 1;
     }
 
-    fn get_pin_count(&self) -> usize {
+    fn get_pin_count(&self) -> RwLockReadGuard<'_, usize> {
         let pin_count = self.pin_count.read().unwrap();
-        *pin_count
+        pin_count
     }
 
     fn set_dirty(&mut self) {
@@ -101,5 +90,70 @@ impl IPage for Page {
     fn set_clean(&mut self) {
         let mut is_dirty = self.is_dirty.write().unwrap();
         *is_dirty = false;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    fn test_set_and_get_data() {
+        let mut page = Page::new(0);
+        assert_eq!(page.is_dirty().to_owned(), false);
+
+        let new_data = [1; PAGE_SIZE];
+        page.set_data(new_data);
+
+        let data = page.get_data();
+
+        assert_eq!(data.len(), PAGE_SIZE);
+        assert_eq!(data[..], new_data);
+        assert_eq!(page.is_dirty().to_owned(), true);
+    }
+
+    #[rstest]
+    fn test_write_data() {
+        let mut page = Page::new(0);
+        assert_eq!(page.is_dirty().to_owned(), false);
+
+        let new_data = [1; 16];
+        page.write_data(32, &new_data);
+
+        let data = page.get_data();
+
+        assert_eq!(data.len(), PAGE_SIZE);
+        assert_eq!(data[0..32], [0; 32]);
+        assert_eq!(data[32..48], new_data);
+        assert_eq!(data[48..PAGE_SIZE], [0; PAGE_SIZE - 48]);
+        assert_eq!(page.is_dirty().to_owned(), true);
+    }
+
+    #[rstest]
+    fn test_get_page_id() {
+        let page = Page::new(123);
+        assert_eq!(page.get_page_id(), 123);
+    }
+
+    #[rstest]
+    fn test_set_dirty_clean() {
+        let mut page = Page::new(0);
+        assert_eq!(page.is_dirty().to_owned(), false);
+        page.set_dirty();
+        assert_eq!(page.is_dirty().to_owned(), true);
+        page.set_clean();
+        assert_eq!(page.is_dirty().to_owned(), false);
+    }
+
+    #[rstest]
+    fn test_increase_pin_count() {
+        let mut page = Page::new(0);
+        assert_eq!(page.get_pin_count().to_owned(), 0);
+        page.increase_pin_count();
+        assert_eq!(page.get_pin_count().to_owned(), 1);
+        page.increase_pin_count();
+        assert_eq!(page.get_pin_count().to_owned(), 2);
     }
 }
