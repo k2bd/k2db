09e40d52321ac26ee70d37f377b27211b883221c
Kevin Duff <kevinkelduff@gmail.com>
2023-04-19 22:31:09 +0000

    Rework page api again, and start building buffer pool manager for real

--- src/dbms/storage/disk/disk_manager.rs
+++ src/dbms/storage/disk/disk_manager.rs
@@ -1,12 +1,17 @@
 #[cfg(test)]
 use mockall::automock;
 
+use crate::dbms::storage::page::PAGE_SIZE;
+
+pub type DiskManagerError = ();
+
 #[cfg_attr(test, automock)]
 pub trait IDiskManager {
-    fn write_page(&mut self, page_id: usize, page: &[u8]) -> Result<(), String>;
-    fn read_page(&mut self, page_id: usize) -> Result<Vec<u8>, String>;
-    fn write_log(&mut self, log: &[u8]) -> Result<(), String>;
-    fn read_log(&mut self, size: usize, offset: usize) -> Result<Vec<u8>, String>;
-    fn allocate_page(&mut self) -> Result<usize, String>;
-    fn deallocate_page(&mut self, page_id: usize) -> Result<(), String>;
+    fn write_page(&mut self, page_id: usize, page: &[u8]) -> Result<(), DiskManagerError>;
+    fn read_page(&mut self, page_id: usize) -> Result<[u8; PAGE_SIZE], DiskManagerError>;
+    fn write_log(&mut self, log: &[u8]) -> Result<(), DiskManagerError>;
+    fn read_log(&mut self, size: usize, offset: usize)
+        -> Result<[u8; PAGE_SIZE], DiskManagerError>;
+    fn allocate_page(&mut self) -> Result<usize, DiskManagerError>;
+    fn deallocate_page(&mut self, page_id: usize) -> Result<(), DiskManagerError>;
 }
