bd9f8e78f7a8cad52130a5f4b06997c372a59f52
Kevin Duff <kevinkelduff@gmail.com>
2023-04-19 22:31:09 +0000

    Rework page api again, and start building buffer pool manager for real

--- src/dbms/buffer/pool_manager/buffer_pool_manager.rs
+++ src/dbms/buffer/pool_manager/buffer_pool_manager.rs
@@ -1,19 +1,23 @@
-#[cfg(test)]
-use mockall::automock;
-
 use std::collections::HashMap;
-use std::sync::{RwLock, RwLockReadGuard};
+use std::sync::{Arc, RwLock, RwLockReadGuard};
 
 use crate::dbms::buffer::replacer::{BufferPoolReplacerError, IBufferPoolReplacer};
-use crate::dbms::storage::disk::IDiskManager;
-use crate::dbms::storage::page::{IPage, Page, PAGE_SIZE, PageData};
+use crate::dbms::storage::disk::{DiskManagerError, IDiskManager};
+use crate::dbms::storage::page::{IPage, Page, PageData, PageError};
 
 pub enum BufferPoolManagerError {
+    /// Unable to free up a page when fetching a page from disk
+    NoFrameAvailable,
     ReplacerError(BufferPoolReplacerError),
+    PageError(PageError),
+    DiskManagerError(DiskManagerError),
 }
 
-pub trait IBufferPoolManager<'a> {
-    fn fetch_page(&self, page_id: usize) -> Result<PageData, BufferPoolManagerError>;
+pub trait IBufferPoolManager {
+    fn fetch_page(
+        &self,
+        page_id: usize,
+    ) -> Result<RwLockReadGuard<Box<dyn IPage>>, BufferPoolManagerError>;
     fn new_page(&self) -> Result<usize, BufferPoolManagerError>;
     fn unpin_page(&self, page_id: usize) -> Result<(), BufferPoolManagerError>;
     fn flush_page(&self, page_id: usize) -> Result<(), BufferPoolManagerError>;
@@ -22,54 +26,119 @@ pub trait IBufferPoolManager<'a> {
 }
 
 struct BufferPoolManager {
-    replacer: RwLock<Box<dyn IBufferPoolReplacer>>,
-    disk_manager: RwLock<Box<dyn IDiskManager>>,
+    replacer: Arc<RwLock<Box<dyn IBufferPoolReplacer>>>,
+    disk_manager: Arc<RwLock<Box<dyn IDiskManager>>>,
     /// page_id -> frame_id
-    page_table: RwLock<HashMap<usize, usize>>,
-    free_frames: RwLock<Vec<usize>>,
-    pages: Vec<Box<dyn IPage>>,
+    // Concurrent mutability on the hashmap
+    page_table: Arc<RwLock<HashMap<usize, usize>>>,
+    // N.B. Concurrent mutability on the array
+    free_frames: Arc<RwLock<Vec<usize>>>,
+    // N.B. Concurrent mutability on each individual page, not the array itself
+    pages: Arc<Vec<RwLock<Box<dyn IPage>>>>,
 }
 
 impl BufferPoolManager {
-    pub fn new(
+    fn new(
         pool_size: usize,
-        replacer: Box<dyn IBufferPoolReplacer>,
-        disk_manager: Box<dyn IDiskManager>,
+        replacer: Arc<RwLock<Box<dyn IBufferPoolReplacer>>>,
+        disk_manager: Arc<RwLock<Box<dyn IDiskManager>>>,
     ) -> BufferPoolManager {
         BufferPoolManager {
-            replacer: RwLock::new(replacer),
-            disk_manager: RwLock::new(disk_manager),
-            page_table: RwLock::new(HashMap::new()),
-            free_frames: RwLock::new((0..pool_size).collect::<Vec<_>>()),
-            pages: (0..pool_size)
-                .map(|i| Box::new(Page::new(i)) as _)
-                .collect(),
+            replacer,
+            disk_manager,
+            page_table: Arc::new(RwLock::new(HashMap::new())),
+            // All frames are free
+            free_frames: Arc::new(RwLock::new((0..pool_size).collect())),
+            // Fill frames with uninitialized pages with no page IDs
+            pages: Arc::new(
+                (0..pool_size)
+                    .map(|_| RwLock::new(Box::new(Page::new(None)) as Box<dyn IPage>))
+                    .collect(),
+            ),
         }
     }
 }
 
-impl<'a> IBufferPoolManager<'a> for BufferPoolManager {
-    fn fetch_page(&self, page_id: usize) -> Result<RwLockReadGuard<'a, [u8; PAGE_SIZE]>, BufferPoolManagerError> {
+impl IBufferPoolManager for BufferPoolManager {
+    fn fetch_page(
+        &self,
+        page_id: usize,
+    ) -> Result<RwLockReadGuard<'_, Box<(dyn IPage)>>, BufferPoolManagerError> {
+        // 1.     Search the page table for the requested page (P).
+        let mut page_table = self.page_table.write().unwrap();
+        let mut replacer = self.replacer.write().unwrap();
+        if let Some(frame_id) = page_table.get(&page_id) {
+            // 1.1    If P exists, pin it and return it immediately.
+            {
+                let mut page = self.pages[*frame_id].write().unwrap();
+                page.increase_pin_count().unwrap();
+                replacer.pin(*frame_id).unwrap();
+            }
+            let page = self.pages[*frame_id].read().unwrap();
+            return Ok(page);
+        }
+
+        // 1.2    If P does not exist, find a replacement page (R) from either the free list or the replacer.
+        //        Note that pages are always found from the free list first.
+        let mut free_frames = self.free_frames.write().unwrap();
+        let new_frame_id = if let Some(f) = free_frames.pop() {
+            f
+        } else {
+            let mut replacer = self.replacer.write().unwrap();
+            let victim_res = replacer.victim();
+            if let Ok(victim) = victim_res {
+                if let Some(frame) = victim {
+                    frame
+                } else {
+                    return Err(BufferPoolManagerError::NoFrameAvailable);
+                }
+            } else {
+                return Err(BufferPoolManagerError::ReplacerError(
+                    victim_res.unwrap_err(),
+                ));
+            }
+        };
+
         {
-            let page_table = self.page_table.read().unwrap();
+            let mut disk_manager = self.disk_manager.write().unwrap();
+            let mut page_to_overwrite = self.pages[new_frame_id].write().unwrap();
 
-            if let Some(frame_id) = page_table.get(&page_id) {
-                // Page ID is already in buffer pool, pin it and return the frame
-                let mut replacer = self.replacer.write().unwrap();
-                let page = &self.pages[*frame_id];
+            if let Some(old_page_id) = page_to_overwrite.get_page_id().unwrap() {
+                // 2.     If R is dirty, write it back to the disk.
+                if page_to_overwrite.is_dirty().unwrap() {
+                    let page_data = page_to_overwrite.get_data().unwrap();
 
-                let pin_res = replacer.pin(*frame_id);
-                if pin_res.is_err() {
-                    return Err(BufferPoolManagerError::ReplacerError(
-                        pin_res.err().unwrap(),
-                    ));
+                    let res = disk_manager.write_page(old_page_id, &page_data);
+                    if let Err(e) = res {
+                        return Err(BufferPoolManagerError::DiskManagerError(e));
+                    }
                 }
 
-                return Ok(page.get_data());
+                // 3.     Delete R from the page table and insert P.
+                page_table.remove(&old_page_id);
+                page_table.insert(page_id, new_frame_id);
+            }
+
+            // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to P.
+            let new_frame_pin_res = replacer.pin(new_frame_id);
+            if let Err(e) = new_frame_pin_res {
+                return Err(BufferPoolManagerError::ReplacerError(e));
+            }
+
+            let new_data_res = disk_manager.read_page(page_id);
+            match new_data_res {
+                Ok(new_page_data) => {
+                    page_to_overwrite
+                        .overwrite(Some(page_id), new_page_data)
+                        .unwrap();
+                }
+                Err(e) => return Err(BufferPoolManagerError::DiskManagerError(e)),
             }
         }
 
-        todo!()
+        // Return a readable copy of the new page
+        let new_page = self.pages[new_frame_id].read().unwrap();
+        Ok(new_page)
     }
 
     fn new_page(&self) -> Result<usize, BufferPoolManagerError> {
--- src/dbms/storage/page/page_type.rs
+++ src/dbms/storage/page/page_type.rs
@@ -1,20 +1,11 @@
-#[cfg(test)]
-use mockall::automock;
-
-use std::sync::{Arc, RwLock};
-
 pub const PAGE_SIZE: usize = 4096;
 
 pub type PageData = [u8; PAGE_SIZE];
 
-#[derive(Debug, PartialEq)]
-pub enum PageError {
-    LockPoisoned(String),
-}
+pub type PageError = ();
 
-#[cfg_attr(test, automock)]
 pub trait IPage {
-    /// Get a thread-safe read-only version of the page's data
+    /// Get a copy of the page's data
     fn get_data(&self) -> Result<PageData, PageError>;
     /// Set the whole content of the page, and set the page to dirty
     fn set_data(&mut self, data: PageData) -> Result<(), PageError>;
@@ -22,7 +13,7 @@ pub trait IPage {
     /// page to dirty
     fn write_data(&mut self, offset: usize, data: &[u8]) -> Result<(), PageError>;
     /// Get the page ID
-    fn get_page_id(&self) -> Result<usize, PageError>;
+    fn get_page_id(&self) -> Result<Option<usize>, PageError>;
     /// Get whether the page is dirty
     fn is_dirty(&self) -> Result<bool, PageError>;
     /// Set the page to dirty
@@ -34,154 +25,78 @@ pub trait IPage {
     /// Get the pin count of the page
     fn get_pin_count(&self) -> Result<usize, PageError>;
     /// Full overwrite page, e.g. when a new page is fetched from disk
-    fn overwrite(&mut self, page_id: usize, data: PageData) -> Result<(), PageError>;
+    fn overwrite(&mut self, page_id: Option<usize>, data: PageData) -> Result<(), PageError>;
 }
 
 #[derive(Clone)]
 pub struct Page {
-    data: Arc<RwLock<PageData>>,
-    page_id: Arc<RwLock<usize>>,
-    pin_count: Arc<RwLock<usize>>,
-    is_dirty: Arc<RwLock<bool>>,
+    data: PageData,
+    page_id: Option<usize>,
+    pin_count: usize,
+    is_dirty: bool,
 }
 
 impl Page {
-    pub fn new(page_id: usize) -> Page {
+    pub fn new(page_id: Option<usize>) -> Page {
         Page {
-            data: Arc::new(RwLock::new([0; PAGE_SIZE])),
-            page_id: Arc::new(RwLock::new(page_id)),
-            pin_count: Arc::new(RwLock::new(0)),
-            is_dirty: Arc::new(RwLock::new(false)),
+            data: [0; PAGE_SIZE],
+            page_id,
+            pin_count: 0,
+            is_dirty: false,
         }
     }
 }
 
 impl IPage for Page {
     fn get_data(&self) -> Result<PageData, PageError> {
-        let page_data_r = self.data.read();
-
-        match page_data_r {
-            Ok(page_data) => Ok(*page_data),
-            Err(_) => Err(PageError::LockPoisoned(
-                "Page data lock poisoned".to_string(),
-            )),
-        }
+        Ok(self.data.clone())
     }
 
     fn set_data(&mut self, data: PageData) -> Result<(), PageError> {
-        self.write_data(0, &data)
+        self.data = data;
+        self.is_dirty = true;
+        Ok(())
     }
 
     fn write_data(&mut self, offset: usize, data: &[u8]) -> Result<(), PageError> {
-        let mut page_data_r = self.data.write();
-        let mut is_dirty_r = self.is_dirty.write();
-
-        match (page_data_r, is_dirty_r) {
-            (Ok(mut page_data), Ok(mut is_dirty)) => {
-                *is_dirty = true;
-                page_data[offset..offset + data.len()].copy_from_slice(data);
-                Ok(())
-            }
-            _ => Err(PageError::LockPoisoned(
-                "Page data lock poisoned".to_string(),
-            )),
-        }
+        self.data[offset..offset + data.len()].copy_from_slice(data);
+        self.is_dirty = true;
+        Ok(())
     }
 
-    fn get_page_id(&self) -> Result<usize, PageError> {
-        let page_id_r = self.page_id.read();
-
-        match page_id_r {
-            Ok(page_id) => Ok(*page_id),
-            Err(_) => Err(PageError::LockPoisoned("Page ID lock poisoned".to_string())),
-        }
+    fn get_page_id(&self) -> Result<Option<usize>, PageError> {
+        Ok(self.page_id)
     }
 
     fn is_dirty(&self) -> Result<bool, PageError> {
-        let is_dirty_r = self.is_dirty.read();
-
-        match is_dirty_r {
-            Ok(is_dirty) => Ok(*is_dirty),
-            Err(_) => Err(PageError::LockPoisoned(
-                "Page dirty lock poisoned".to_string(),
-            )),
-        }
+        Ok(self.is_dirty)
     }
 
     fn increase_pin_count(&mut self) -> Result<(), PageError> {
-        let mut pin_count_r = self.pin_count.write();
-
-        match pin_count_r {
-            Ok(mut pin_count) => {
-                *pin_count += 1;
-                Ok(())
-            }
-            Err(_) => Err(PageError::LockPoisoned(
-                "Page pin count lock poisoned".to_string(),
-            )),
-        }
+        self.pin_count += 1;
+        Ok(())
     }
 
     fn get_pin_count(&self) -> Result<usize, PageError> {
-        let pin_count_r = self.pin_count.read();
-
-        match pin_count_r {
-            Ok(pin_count) => Ok(*pin_count),
-            Err(_) => Err(PageError::LockPoisoned(
-                "Page pin count lock poisoned".to_string(),
-            )),
-        }
+        Ok(self.pin_count)
     }
 
     fn set_dirty(&mut self) -> Result<(), PageError> {
-        let mut is_dirty = self.is_dirty.write();
-
-        match is_dirty {
-            Ok(mut is_dirty) => {
-                *is_dirty = true;
-                Ok(())
-            }
-            Err(_) => Err(PageError::LockPoisoned(
-                "Page dirty lock poisoned".to_string(),
-            )),
-        }
+        self.is_dirty = true;
+        Ok(())
     }
 
     fn set_clean(&mut self) -> Result<(), PageError> {
-        let mut is_dirty = self.is_dirty.write();
-
-        match is_dirty {
-            Ok(mut is_dirty) => {
-                *is_dirty = false;
-                Ok(())
-            }
-            Err(_) => Err(PageError::LockPoisoned(
-                "Page dirty lock poisoned".to_string(),
-            )),
-        }
+        self.is_dirty = false;
+        Ok(())
     }
 
-    fn overwrite(&mut self, page_id: usize, data: PageData) -> Result<(), PageError> {
-        let mut page_id_writer = self.page_id.write();
-        let mut page_data_writer = self.data.write();
-        let mut is_dirty_writer = self.is_dirty.write();
-        let mut pin_count_writer = self.pin_count.write();
-
-        match (
-            page_id_writer,
-            page_data_writer,
-            is_dirty_writer,
-            pin_count_writer,
-        ) {
-            (Ok(mut page_id_w), Ok(mut page_data), Ok(mut is_dirty), Ok(mut pin_count)) => {
-                *page_id_w = page_id;
-                *page_data = data;
-                *is_dirty = false;
-                *pin_count = 0;
-                Ok(())
-            }
-            _ => Err(PageError::LockPoisoned("Page lock poisoned".to_string())),
-        }
+    fn overwrite(&mut self, page_id: Option<usize>, data: PageData) -> Result<(), PageError> {
+        self.page_id = page_id;
+        self.data = data;
+        self.pin_count = 0;
+        self.is_dirty = false;
+        Ok(())
     }
 }
 
@@ -192,11 +107,12 @@ mod tests {
 
     #[rstest]
     fn test_set_and_get_data() {
-        let mut page = Page::new(0);
+        let mut page = Page::new(Some(0));
         assert_eq!(page.is_dirty().unwrap(), false);
 
         let new_data = [1; PAGE_SIZE];
-        page.set_data(new_data);
+        let res = page.set_data(new_data);
+        assert_eq!(res, Ok(()));
 
         let data = page.get_data().unwrap();
 
@@ -207,11 +123,12 @@ mod tests {
 
     #[rstest]
     fn test_write_data() {
-        let mut page = Page::new(0);
+        let mut page = Page::new(Some(0));
         assert_eq!(page.is_dirty().unwrap(), false);
 
         let new_data = [1; 16];
-        page.write_data(32, &new_data);
+        let res = page.write_data(32, &new_data);
+        assert_eq!(res, Ok(()));
 
         let data = page.get_data().unwrap();
 
@@ -224,27 +141,31 @@ mod tests {
 
     #[rstest]
     fn test_get_page_id() {
-        let page = Page::new(123);
-        assert_eq!(page.get_page_id().unwrap(), 123);
+        let page = Page::new(Some(123));
+        assert_eq!(page.get_page_id().unwrap(), Some(123));
     }
 
     #[rstest]
     fn test_set_dirty_clean() {
-        let mut page = Page::new(0);
+        let mut page = Page::new(Some(0));
         assert_eq!(page.is_dirty().unwrap(), false);
-        page.set_dirty();
+        let res1 = page.set_dirty();
+        assert_eq!(res1, Ok(()));
         assert_eq!(page.is_dirty().unwrap(), true);
-        page.set_clean();
+        let res2 = page.set_clean();
+        assert_eq!(res2, Ok(()));
         assert_eq!(page.is_dirty().unwrap(), false);
     }
 
     #[rstest]
     fn test_increase_pin_count() {
-        let mut page = Page::new(0);
+        let mut page = Page::new(Some(0));
         assert_eq!(page.get_pin_count().unwrap(), 0);
-        page.increase_pin_count();
+        let res1 = page.increase_pin_count();
+        assert_eq!(res1, Ok(()));
         assert_eq!(page.get_pin_count().unwrap(), 1);
-        page.increase_pin_count();
+        let res2 = page.increase_pin_count();
+        assert_eq!(res2, Ok(()));
         assert_eq!(page.get_pin_count().unwrap(), 2);
     }
 }
