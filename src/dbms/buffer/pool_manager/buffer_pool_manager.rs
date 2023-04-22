f538a799163b0af3d7f967bfd00e4f854667675b
Kevin Duff <kevinkelduff@gmail.com>
2023-04-22 22:24:34 +0000

    Initial implementation of new_page as well

--- src/dbms/buffer/pool_manager/buffer_pool_manager.rs
+++ src/dbms/buffer/pool_manager/buffer_pool_manager.rs
@@ -5,6 +5,7 @@ use crate::dbms::buffer::replacer::{BufferPoolReplacerError, IBufferPoolReplacer
 use crate::dbms::storage::disk::{DiskManagerError, IDiskManager};
 use crate::dbms::storage::page::{IPage, Page, PageData, PageError};
 
+#[derive(Debug)]
 pub enum BufferPoolManagerError {
     /// Unable to free up a page when fetching a page from disk
     NoFrameAvailable,
@@ -24,7 +25,7 @@ pub trait IBufferPoolManager {
     /// Fetch the requested page as writable from the buffer pool.
     fn fetch_page_writable(&self, page_id: usize) -> Result<WritablePage, BufferPoolManagerError>;
     /// Creates a new page in the buffer pool, returning it as writable.
-    fn new_page(&self) -> Result<WritablePage, BufferPoolManagerError>;
+    fn new_page(&self) -> Result<Option<WritablePage>, BufferPoolManagerError>;
     /// Unpin the target page from the buffer pool.
     fn unpin_page(&self, page_id: usize, mark_dirty: bool) -> Result<(), BufferPoolManagerError>;
     /// Flushes the target page to disk.
@@ -68,6 +69,61 @@ impl BufferPoolManager {
         }
     }
 
+    fn get_freeable_frame_id(
+        &self,
+        replacer: &mut RwLockWriteGuard<Box<dyn IBufferPoolReplacer>>,
+    ) -> Result<usize, BufferPoolManagerError> {
+        let mut free_frames = self.free_frames.write().unwrap();
+        if let Some(f) = free_frames.pop() {
+            Ok(f)
+        } else {
+            let victim_res = replacer.victim();
+            if let Ok(victim) = victim_res {
+                if let Some(frame) = victim {
+                    Ok(frame)
+                } else {
+                    Err(BufferPoolManagerError::NoFrameAvailable)
+                }
+            } else {
+                Err(BufferPoolManagerError::ReplacerError(
+                    victim_res.unwrap_err(),
+                ))
+            }
+        }
+    }
+
+    /// Flush a page to disk and prep it for a new page
+    fn swap_frame(
+        &self,
+        frame_id: usize,
+        page_id: usize,
+        disk_manager: &mut RwLockWriteGuard<Box<dyn IDiskManager>>,
+        replacer: &mut RwLockWriteGuard<Box<dyn IBufferPoolReplacer>>,
+        page_table: &mut RwLockWriteGuard<HashMap<usize, usize>>,
+        page: &mut RwLockWriteGuard<Box<dyn IPage>>,
+    ) -> Result<(), BufferPoolManagerError> {
+        if let Some(old_page_id) = page.get_page_id().unwrap() {
+            if page.is_dirty().unwrap() {
+                let page_data = page.get_data().unwrap();
+
+                let res = disk_manager.write_page(old_page_id, &page_data);
+                if let Err(e) = res {
+                    return Err(BufferPoolManagerError::DiskManagerError(e));
+                }
+            }
+
+            page_table.remove(&old_page_id);
+            page_table.insert(page_id, frame_id);
+        }
+
+        let new_frame_pin_res = replacer.pin(frame_id);
+        if let Err(e) = new_frame_pin_res {
+            return Err(BufferPoolManagerError::ReplacerError(e));
+        }
+
+        Ok(())
+    }
+
     /// Fetch a page, from disk if needed, and return its frame ID
     fn fetch_page_frame(&self, page_id: usize) -> Result<usize, BufferPoolManagerError> {
         // 1.     Search the page table for the requested page (P).
@@ -86,49 +142,26 @@ impl BufferPoolManager {
 
         // 1.2    If P does not exist, find a replacement page (R) from either the free list or the replacer.
         //        Note that pages are always found from the free list first.
-        let mut free_frames = self.free_frames.write().unwrap();
-        let new_frame_id = if let Some(f) = free_frames.pop() {
-            f
-        } else {
-            let victim_res = replacer.victim();
-            if let Ok(victim) = victim_res {
-                if let Some(frame) = victim {
-                    frame
-                } else {
-                    return Err(BufferPoolManagerError::NoFrameAvailable);
-                }
-            } else {
-                return Err(BufferPoolManagerError::ReplacerError(
-                    victim_res.unwrap_err(),
-                ));
-            }
-        };
+        let new_frame_id = self.get_freeable_frame_id(&mut replacer).unwrap();
 
         {
             let mut disk_manager = self.disk_manager.write().unwrap();
             let mut page_to_overwrite = self.pages[new_frame_id].write().unwrap();
 
-            if let Some(old_page_id) = page_to_overwrite.get_page_id().unwrap() {
-                // 2.     If R is dirty, write it back to the disk.
-                if page_to_overwrite.is_dirty().unwrap() {
-                    let page_data = page_to_overwrite.get_data().unwrap();
-
-                    let res = disk_manager.write_page(old_page_id, &page_data);
-                    if let Err(e) = res {
-                        return Err(BufferPoolManagerError::DiskManagerError(e));
-                    }
-                }
-
-                // 3.     Delete R from the page table and insert P.
-                page_table.remove(&old_page_id);
-                page_table.insert(page_id, new_frame_id);
-            }
-
+            // 2.     If R is dirty, write it back to the disk.
+            // 3.     Delete R from the page table and insert P.
             // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to P.
-            let new_frame_pin_res = replacer.pin(new_frame_id);
-            if let Err(e) = new_frame_pin_res {
-                return Err(BufferPoolManagerError::ReplacerError(e));
-            }
+            match self.swap_frame(
+                new_frame_id,
+                page_id,
+                &mut disk_manager,
+                &mut replacer,
+                &mut page_table,
+                &mut page_to_overwrite,
+            ) {
+                Ok(_) => {}
+                Err(e) => return Err(e),
+            };
 
             let new_data_res = disk_manager.read_page(page_id);
             match new_data_res {
@@ -142,6 +175,19 @@ impl BufferPoolManager {
         }
 
         Ok(new_frame_id)
+    }
+
+    fn find_matching_page(
+        &self,
+        predicate: impl Fn(&dyn IPage) -> bool,
+    ) -> Result<Option<usize>, BufferPoolManagerError> {
+        for (page_id, frame_id) in self.page_table.read().unwrap().iter() {
+            let page = self.pages[*frame_id].read().unwrap();
+            if predicate(page.as_ref()) {
+                return Ok(Some(*page_id));
+            }
+        }
+        Ok(None)
     }
 }
 
@@ -160,14 +206,59 @@ impl IBufferPoolManager for BufferPoolManager {
         }
     }
 
-    fn new_page(&self) -> Result<WritablePage, BufferPoolManagerError> {
-        // 0.   Make sure you call DiskManager::AllocatePage!
-        // 1.   If all the pages in the buffer pool are pinned, return nullptr.
-        // 2.   Pick a victim page P from either the free list or the replacer. Always pick from the free list first.
-        // 3.   Update P's metadata, zero out memory and add P to the page table.
-        // 4.   Set the page ID output parameter. Return a pointer to P.
+    fn new_page(&self) -> Result<Option<WritablePage>, BufferPoolManagerError> {
+        let mut page_table = self.page_table.write().unwrap();
+        let mut disk_manager = self.disk_manager.write().unwrap();
+        let mut replacer = self.replacer.write().unwrap();
 
-        todo!()
+        // 1.   If all the pages in the buffer pool are pinned, return nullptr.
+        match self.find_matching_page(|page| page.get_pin_count() == Ok(0)) {
+            Ok(Some(_)) => {}
+            Ok(None) => {
+                return Ok(None);
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        };
+
+        // 0.   Make sure you call DiskManager::AllocatePage!
+        let new_page_id = match disk_manager.allocate_page() {
+            Ok(page_id) => page_id,
+            Err(e) => return Err(BufferPoolManagerError::DiskManagerError(e)),
+        };
+
+        // 2.   Pick a victim page P from either the free list or the replacer. Always pick from the free list first.
+        let frame_id = self.get_freeable_frame_id(&mut replacer).unwrap();
+        let mut page_to_overwrite = self.pages[frame_id].write().unwrap();
+        match self.swap_frame(
+            frame_id,
+            new_page_id,
+            &mut disk_manager,
+            &mut replacer,
+            &mut page_table,
+            &mut page_to_overwrite,
+        ) {
+            Ok(_) => {}
+            Err(e) => return Err(e),
+        };
+
+        // 3.   Update P's metadata, zero out memory and add P to the page table.
+        match page_to_overwrite.clear() {
+            Ok(_) => {}
+            Err(e) => return Err(BufferPoolManagerError::PageError(e)),
+        }
+        match page_to_overwrite.increase_pin_count() {
+            Ok(_) => {}
+            Err(e) => return Err(BufferPoolManagerError::PageError(e)),
+        }
+        match replacer.pin(frame_id) {
+            Ok(_) => {}
+            Err(e) => return Err(BufferPoolManagerError::ReplacerError(e)),
+        }
+
+        // 4.   Set the page ID output parameter. Return a pointer to P.
+        Ok(Some(page_to_overwrite))
     }
 
     fn unpin_page(&self, page_id: usize, mark_dirty: bool) -> Result<(), BufferPoolManagerError> {
